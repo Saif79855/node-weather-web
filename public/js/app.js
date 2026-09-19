6d5e6d07ffd982c6f8bbd8b915f351ea3e47c61e
Abdul Sajjad <8saifkhan@gmail.com>
2026-09-19 13:41:02 +0530

    cleaned code a little bit

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
-
+  console.log(location);
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
